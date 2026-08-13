fab31ceaf5b827c03578aa846c425b5f83a1a6ef
Ram Shrivastava <userblu133@gmail.com>
2026-08-13 21:21:07 +0530

    Added Solution for Bubble_sort_array

---
+++ Arrays/1D-Arrays/Bubble_sort_array.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int j = 0; j < n - 1; j++)
+    {
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i - 1] > arr[i])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+            }
+        }
+    }
+    for (int i : arr)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
